93b23d9e65992b207e1a17f888beb0096aa54328
luojinagyu <724750614@qq.com>
2024-02-29 11:30:24 +0800

    自定义listpages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import {
   Admin,
   Resource,
-  ListGuesser,
-  EditGuesser,
   ShowGuesser,
 } from "react-admin";
+import PeopleIcon from '@mui/icons-material/People';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+
+import { UserList,UserEdit } from "./pages/users"
+
 import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
-
+import { MyLayout } from './MyLayout';
 export const App = () => (
-  <Admin dataProvider={dataProvider} authProvider={authProvider}>
+  <Admin dataProvider={dataProvider} authProvider={authProvider} layout={MyLayout}>
     <Resource
-      name="posts"
-      list={ListGuesser}
-      edit={EditGuesser}
-      show={ShowGuesser}
-    />
-    <Resource
-      name="comments"
-      list={ListGuesser}
-      edit={EditGuesser}
-      show={ShowGuesser}
+      name="users"
+      edit={UserEdit}
+      list={UserList}
+      icon={PeopleIcon}
     />
   </Admin>
 );
