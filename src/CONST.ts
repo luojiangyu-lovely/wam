1fe61f77e135fb85e191b9dfb83dd9c1bbf0f725
luojinagyu <724750614@qq.com>
2024-03-29 15:41:19 +0800

    常量文件

---
+++ src/CONST.ts
@@ -0,0 +1,78 @@
+
+export const qdOption: any[] = [
+    { id: '1', name: 'TabTab' },
+    { id: '2', name: '好游快报' }
+
+]
+export const qdOptionObj: any = {
+    "1": 'TabTab',
+    "2": '好游快报'
+}
+
+export const itemsOption: any[] = [
+    {
+        value: 1,
+        label: '宝石',
+    },
+    {
+        value: 2,
+        label: '黄金',
+    },
+    {
+        value: 4,
+        label: '木材',
+    },
+    {
+        value: 5,
+        label: '石料',
+    },
+    {
+        value: 6,
+        label: '铁矿',
+    },
+    {
+        value: 7,
+        label: '粮食',
+    },
+    {
+        value: 8,
+        label: '战略卷轴',
+    },
+    {
+        value: 13,
+        label: '抽卡抵用卷',
+    }
+]
+export const itemsObj: any = {
+    1: '宝石',
+    2: '黄金',
+    4: '木材',
+    5: '石料',
+    6: '铁矿',
+    7: '粮食',
+    8: '战略卷轴',
+    13: '抽卡抵用卷'
+}
+
+export const premissObj: any = {
+    'users': '账号模块',
+    'roles': '角色查询',
+    "bans": '封禁系统',
+    'addMails': '新增邮件',
+    'examineMails': '封禁系统',
+    "raceLamps":'跑马灯',
+    "notice":'公告'
+}
+
+export const server = [
+    { id: "10001", name: '测试服' }
+]
+
+export const sendStatus = [
+    { id: 2, name: '已作废' },
+    { id: 1, name: '已发送' },
+    { id: 0, name: '未发送' },
+
+]
+export const sendStatusObj ={ 2: '已作废', 1: '已发送', 0: '未发送' }
+
